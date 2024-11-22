6f36b778e8e25e9367d6955b0da70e4a5db28887
artartar <arturo.simpson.jr@afs.com>
2024-11-22 10:19:13 -0500

    "Open file using the os package. Read contents of the file with the encoding/csv packge. Print content as an initial test."

--- quiz_game.go
+++ quiz_game.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	// "encoding/csv"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	// Open the file for reading by the csv package
+	open_quiz_file, open_err := os.Open("problems.csv")
+	if open_err != nil {
+		log.Fatal(open_err)
+	}
+	defer open_quiz_file.Close()
+
+	// Read content of file
+	read_quiz := csv.NewReader(open_quiz_file)
+	questions, read_err := read_quiz.ReadAll()
+	if read_err != nil {
+		log.Fatal(read_err)
+	}
+
+	fmt.Print(questions)
 }
