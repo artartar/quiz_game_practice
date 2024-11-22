af407d8d7711ff3d99e4e40c6ca89fcfb36b0514
artartar <artsim3088@gmail.com>
2024-11-22 14:32:12 -0500

    Final solution for part 1. Added a for loop to iterate through the questions. I request user input and compare to the answer of each question.

--- quiz_game.go
+++ quiz_game.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -9,8 +10,16 @@ import (
 
 func main() {
 
+	const defaultQuiz = "problems.csv"
+	var score = 0
+	var userInput string
+	// Create flag so user can provide quiz file with custom name
+	var quizFlag = flag.String("file", defaultQuiz, "name of the custom quiz file")
+
+	flag.Parse()
+
 	// Open the file for reading by the csv package
-	open_quiz_file, open_err := os.Open("problems.csv")
+	open_quiz_file, open_err := os.Open(*quizFlag)
 	if open_err != nil {
 		log.Fatal(open_err)
 	}
@@ -23,5 +32,14 @@ func main() {
 		log.Fatal(read_err)
 	}
 
-	fmt.Print(questions)
+	for i := range len(questions) {
+		fmt.Printf("Question %d: %v", i+1, questions[i][0])
+		fmt.Scan(&userInput)
+		fmt.Print("\n")
+
+		if userInput == questions[i][1] {
+			score += 1
+		}
+	}
+	fmt.Printf("Total questions %d\nYour score: %d/%d", len(questions), score, len(questions))
 }
